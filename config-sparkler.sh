6cf775be253d75fde654ada7d669cd27ac88c0b0
Affe Null <affenull2345@gmail.com>
2024-06-10 21:12:29 +0200

    Add msm-port-opener

--- config-sparkler.sh
+++ config-sparkler.sh
@@ -10,6 +10,8 @@ need_repo packages/mipi-dbi-configs \
     https://github.com/sailfish-on-sparkler/mipi-dbi-configs.git
 need_repo packages/msm-firmware-loader \
     https://github.com/sailfish-on-sparkler/msm-firmware-loader-rpm.git
+need_repo packages/msm-port-opener \
+    https://github.com/sailfish-on-sparkler/msm-port-opener.git
 need_repo packages/kernel-adaptation-msm8916 \
     https://github.com/sailfish-on-sparkler/kernel-adaptation-msm8916.git
 need_repo packages/device-configuration-sparkler \
@@ -17,4 +19,4 @@ need_repo packages/device-configuration-sparkler \
 need_repo packages/device-sparkler-img-boot \
     https://github.com/sailfish-on-sparkler/device-sparkler-img-boot.git
 
-DEFAULT_MW="qrtr rmtfs mipi-dbi-configs msm-firmware-loader"
+DEFAULT_MW="qrtr rmtfs mipi-dbi-configs msm-firmware-loader msm-port-opener"
